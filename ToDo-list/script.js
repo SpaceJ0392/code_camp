ba125a02d7cdd18af09917d5db590aba070d6bf4
cosmosJ <jwj0392@gmail.com>
2024-03-18 21:56:35 +0900

    code camp JS study (session 7 cont)

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,20 +1,30 @@
+const createTodo = function (todoInput) {
+    /** JS로 HTML 태그 생성 **/
+    const newLi = document.createElement("li");
+    const newSpan = document.createElement("span");
+    const newBtn = document.createElement("button");
+
+    /** addEventlistener */
+    newBtn.addEventListener("click", () => {
+        newLi.classList.toggle("complete"); //li 태그에 새로운 class 추가
+    });
+
+    /** appendChild */
+    newSpan.textContent = todoInput.value;
+    newLi.appendChild(newBtn);
+    newLi.appendChild(newSpan); // 태그 자녀화
+
+    //display
+    document.querySelector("#Todo-list").appendChild(newLi);
+};
+
 const keyCodeCheck = function () {
     const todoInput = document.querySelector("#Todo-input");
 
     /** JS로 키보드 이벤트 받기 */
     window.onkeydown = (event) => {
         if (event.keyCode === 13 && todoInput.value !== "") {
-            /** JS로 HTML 태그 생성 **/
-            const newLi = document.createElement("li");
-            const newSpan = document.createElement("span");
-
-            /** appendChild */
-            newSpan.textContent = todoInput.value;
-            newLi.appendChild(newSpan); // 태그 자녀화
-
-            //display
-            document.querySelector("#Todo-list").appendChild(newLi);
-
+            createTodo(todoInput);
             todoInput.value = ""; //입력 초기화
         }
     };
